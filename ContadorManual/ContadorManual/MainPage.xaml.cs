d4b48c7d0a237f413a489a1161012bc6e09e29e4
Verenice Ceballos Zubieta <14470416@itcampeche.edu.mx>
2021-10-29 23:52:56 -0500

    Agrega la clase Contador y realiza el conteo utilizando una instancia de esta clase.

--- ContadorManual/ContadorManual/MainPage.xaml.cs
+++ ContadorManual/ContadorManual/MainPage.xaml.cs
@@ -5,29 +5,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using ContadorManual.Model;
 
 namespace ContadorManual
 {
     public partial class MainPage : ContentPage
     {
-        private int Conteo;
+        private Contador _Contador;
         public MainPage()
         {
             InitializeComponent();
-            Conteo = 0;
-            ConteoLabel.Text = Conteo.ToString();
+            _Contador = new Contador();
+            ConteoLabel.Text = _Contador.Conteo.ToString();
         }
 
         private void ContarButton_Clicked(object sender, EventArgs e)
         {
-            Conteo++;
-            ConteoLabel.Text = Conteo.ToString();
+            _Contador.Contar();
+            ConteoLabel.Text = _Contador.Conteo.ToString();
         }
 
         private void ReiniciarButton_Clicked(object sender, EventArgs e)
         {
-            Conteo = 0;
-            ConteoLabel.Text = Conteo.ToString();
+            _Contador.ReiniciarConteo ();
+            ConteoLabel.Text = _Contador.Conteo.ToString();
         }
     }
 }
---
+++ ContadorManual/ContadorManual/Model/Contador.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContadorManual.Model
+{
+    public class Contador
+    {
+        private int _Conteo;
+     
+        public int Conteo
+        {
+            get => _Conteo;
+            set
+            {
+                if (_Conteo != value)
+                {
+                    _Conteo = value;
+                }
+            }
+
+        }
+
+        public Contador()
+        {
+            ReiniciarConteo();
+        }
+
+        public void Contar()
+        {
+            Conteo++;
+        }
+
+        public void ReiniciarConteo()
+        {
+            Conteo = 0;
+        }
+
+    }
+}
