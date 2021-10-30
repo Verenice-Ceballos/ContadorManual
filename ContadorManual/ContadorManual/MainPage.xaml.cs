fcf8e05bb95a75033ac1acfe427fa97550237df4
Verenice Ceballos Zubieta <14470416@itcampeche.edu.mx>
2021-10-30 00:11:50 -0500

    Implementa la sincronización utilizando Data Binding

--- ContadorManual/ContadorManual/MainPage.xaml.cs
+++ ContadorManual/ContadorManual/MainPage.xaml.cs
@@ -16,19 +16,20 @@ namespace ContadorManual
         {
             InitializeComponent();
             _Contador = new Contador();
-            ConteoLabel.Text = _Contador.Conteo.ToString();
+           // ConteoLabel.Text = _Contador.Conteo.ToString();
+            ConteoLabel.BindingContext = _Contador;
         }
 
         private void ContarButton_Clicked(object sender, EventArgs e)
         {
             _Contador.Contar();
-            ConteoLabel.Text = _Contador.Conteo.ToString();
+            //ConteoLabel.Text = _Contador.Conteo.ToString();
         }
 
         private void ReiniciarButton_Clicked(object sender, EventArgs e)
         {
             _Contador.ReiniciarConteo ();
-            ConteoLabel.Text = _Contador.Conteo.ToString();
+            //ConteoLabel.Text = _Contador.Conteo.ToString();
         }
     }
 }
--- ContadorManual/ContadorManual/Model/Contador.cs
+++ ContadorManual/ContadorManual/Model/Contador.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 
 namespace ContadorManual.Model
 {
-    public class Contador
+    public class Contador : INotifyPropertyChanged
+
     {
         private int _Conteo;
-     
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
         public int Conteo
         {
             get => _Conteo;
@@ -16,6 +23,7 @@ namespace ContadorManual.Model
                 if (_Conteo != value)
                 {
                     _Conteo = value;
+                    OnPropertyChanged("Conteo");
                 }
             }
 
@@ -35,6 +43,14 @@ namespace ContadorManual.Model
         {
             Conteo = 0;
         }
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
 
     }
 }
